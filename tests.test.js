7812a5930ef2db1541c64eb5beb9c987fb509a54
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-03-20 01:32:48 +0300

    add second task

--- tests.test.js
+++ tests.test.js
@@ -1,6 +1,7 @@
 const sumHandshake = require('./task.js');
+const sumHandshakes = require('./task.js');
 
-describe("Amount of handshakes:", () => {
+describe("Amount of handshakes (task 1):", () => {
     test("check with exact equality", () => {
         expect(sumHandshake(10)).toBe(45);
     })
@@ -23,5 +24,19 @@ describe("Amount of handshakes:", () => {
 
     test("amount of handshakes is less than...", () => {
         expect(sumHandshake(7)).toBeLessThan(28);
+    })
+})
+
+describe("Amount of handshakes (task 2):", () => {
+    test("check with exact equality", () => {
+        expect(sumHandshakes(5)).toBe(10);
+    })
+
+    test("check the value of an object", () => {
+        expect(sumHandshakes(4)).toEqual(6);
+    })
+
+    test("amount of handshakes is more than...", () => {
+        expect(sumHandshakes(6)).toBeGreaterThan(14);
     })
 })
